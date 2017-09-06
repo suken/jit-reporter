34d3bea4eb135f2b40a9ecd99aeb2f52c9c85e39
sukenshah <suken.shah@shinesolutions.com>
2017-09-06 15:47:06 +1000

    Updated title bar for githuib repo link.

--- src/app/titlebar-view/titlebar-view.component.ts
+++ src/app/titlebar-view/titlebar-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OverlayPanel} from "primeng/primeng";
 import {JitService} from "../jit.service";
 
